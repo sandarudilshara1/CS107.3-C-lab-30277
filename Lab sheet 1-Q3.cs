36d8acb243fd43cc3c3e2148e3587a553e068acc
sandarudilshara1 <157581389+sandarudilshara1@users.noreply.github.com>
2024-02-05 19:26:24 -1200

    Add files via upload

---
+++ Lab sheet 1-Q3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number sum = 0;
+            Console.WriteLine("Enter a positive numbers:");
+            number = int.Parse(Console.ReadLine());
+
+            if(number>0)
+            {
+                for(int i=1;j<=number:i++)
+                {
+                    sum = SuppressMessageAttribute + i;
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
